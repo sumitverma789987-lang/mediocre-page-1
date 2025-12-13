c429d4549c08794b63a88e1656ad0e8ebd7f346d
sumitverma789987-lang <sumitverma789987@gmail.com>
2025-12-13 12:10:04 +0530

    fixed

--- script.js
+++ script.js
@@ -4,22 +4,10 @@
 
   
   menu.classList.add('translate-x-[-100%]'); 
-  
-
-  
   menuBtn.addEventListener('click', () => {
     menu.classList.toggle('translate-x-0'); 
     menu.classList.toggle('translate-x-[-100%]'); 
   });
-
-  
-  document.addEventListener('click', (e) => {
-    if (!menu.contains(e.target) && !menuBtn.contains(e.target)) {
-      menu.classList.add('translate-x-[-100%]');
-      menu.classList.remove('translate-x-0');
-    }
-  });
-
 
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("open");
